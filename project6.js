bdaaf31ae8cb02e873e0a290892453a449f8a934
skneha9976 <156420436+skneha9976@users.noreply.github.com>
2024-04-14 20:05:09 +0530

    Add files via upload

---
+++ project6.js
@@ -0,0 +1,10 @@
+const images= document.querySelectorAll('.box-row')
+const p2= document.querySelector('.p2')
+
+console.log(images)
+
+function slideChange(){
+    p2.addEventListener('click', function(event){
+        return images
+    })
+}
